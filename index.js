ead039f18d8976c75d24b5ae0b6cb1e63a6a224b
sonusainju <sainjusonu@gmail.com>
2020-04-12 13:44:18 -0600

    Added nodemon pkg

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const path = require("path");
 
 app.use("/assets", express.static('public'));
 
+// root
 app.get("/", function(req, res) {
     res.status(200).sendFile(path.join(__dirname, "index.html"));
 });
 
+// Order page
 app.get("/order", function(req, res) {
     res.status(200).sendFile(path.join(__dirname, "HTML/order.html"));
 });
