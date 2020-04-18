14e7aa888cd97ba638858ee72ea4f70508269ddd
sonusainju <sainjusonu@gmail.com>
2020-04-18 11:16:51 -0600

    login and register page

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const path = require("path");
 const express = require("express");
 const app = express();
-const path = require("path");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require('express-flash');
+const session = require('express-session');
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport,
+    email => users.find(user=> user.email === email)
+    );
+
+const users = []
 
 app.use("/assets", express.static('public'));
+app.use(express.urlencoded({extended:false}));
+
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+
+// To make our variables persistant across the entire session
+app.use(passport.session())
 
 // root
 app.get("/", function(req, res) {
@@ -12,6 +41,40 @@ app.get("/", function(req, res) {
 // Order page
 app.get("/order", function(req, res) {
     res.status(200).sendFile(path.join(__dirname, "HTML/order.html"));
+});
+
+// login page
+app.get("/login", function(req, res) {
+    res.status(200).sendFile(path.join(__dirname, "HTML/login.html"));
+});
+
+// Register page
+app.get("/register", function(req, res) {
+    res.status(200).sendFile(path.join(__dirname, "HTML/register.html"));
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+
+}));
+
+app.post('/register', async(req,res)=> {
+
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id:Date.now().toString(),
+            name:req.body.name,
+            email:req.body.email,
+            password:hashedPassword
+        })
+        res.redirect('/login');
+    } catch{
+        res.redirect('/register');
+    }
+    console.log(users);
+
 });
 
 app.listen(3000, function() {
